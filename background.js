8816fe3c50bfb72e9123f618e202c2402b75a813
Alejandro Tardín <alejandro@tardin.com>
2014-02-21 19:43:05 +0000

    Added Oauth to the chrome extension and awesome style

---
+++ background.js
@@ -0,0 +1,13 @@
+var oauth = ChromeExOAuth.initBackgroundPage({
+    request_url: 'https://trello.com/1/OAuthGetRequestToken',
+    authorize_url: 'https://trello.com/1/OAuthAuthorizeToken',
+    access_url: 'https://trello.com/1/OAuthGetAccessToken',
+    consumer_key: '95118f19be596f2e5a271ed37449132d',
+    consumer_secret: '1985e493c855f80253edff396c8f809264d304f478296a4f762a0b446651e31e',
+    scope: 'read,write',
+    app_name: 'Car to Trello'
+});
+
+oauth.authorize(function() {
+    console.log('COOL');
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,19 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, {}, function(car) {
-		$('#car .name').text(car.name);
-		$('#car #price').text(car.price);
-		$('#car img').attr('src', car.image);
-	});
-});
+	chrome.tabs.sendMessage(tabs[0].id, {}, function(car) {		
+		if (car) {
+			$('#car .name').text(car.name);
+			$('#car #price').text(car.price);
+			$('#car img').attr('src', car.image);
 
-$('#button').click(function() {
+			$('#button').click(function() {
+				chrome.runtime.sendMessage(car, function(err) {
+					$('#car').fadeOut(function() { window.close();});
+				});
+			});			
+			$('#car').removeClass('hide');
+		} else {
+			$('#error').removeClass('hide');			
+		}
+		$('#loading').addClass('hide');
+	});
 });
