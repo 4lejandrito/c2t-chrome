536687be36c0c66e54ac2a93fad82aa6774b5482
Alejandro Tardin <alejandro.tardin@wds.co>
2014-02-21 14:09:36 +0000

    Better styles

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,7 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	chrome.tabs.sendMessage(tabs[0].id, {}, function(car) {
 		$('#car .name').text(car.name);
+		$('#car #price').text(car.price);
 		$('#car img').attr('src', car.image);
 	});
 });
