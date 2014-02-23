8b1a041afa1b9756eb9980af20bfa0df656916d0
Alejandro Tardín <alejandro@tardin.com>
2014-02-23 19:24:56 +0000

    Name now comes from make + model

--- content/autotrader.js
+++ content/autotrader.js
@@ -1,8 +1,11 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+
+	var result = $('[name="bannerMetaData"]').attr('content').match(/make=(.+),model=(.+),mileage.*/);
+
     sendResponse({
 	 	url: location.href,
 	 	price: parseInt($('#price').text().substring(1).replace(',', '')),
-	 	name: $('#fullPageMainTitle').text(),
+	 	name: result[1] + ' ' + result[2],
 	 	image: $('#fpa-showroomCover').attr('src')
  	});
 });
--- content/gumtree.js
+++ content/gumtree.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     sendResponse({
         url: location.href,
         price: parseInt($('[itemprop="price"]').text().substring(1).replace(',', '')),
-        name: $('[itemprop="name"]').text(),
+        name: $('#vip-attributes li:eq(0) p').text() + ' ' +  $('#vip-attributes li:eq(1) p').text(),
         image: $('[itemprop="image"]').attr('content')
     });    
 });
