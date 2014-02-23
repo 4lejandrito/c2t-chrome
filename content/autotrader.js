ee1675e7302f446ed4fa50c4a787eca8ce20d77f
Alejandro Tardín <alejandro@tardin.com>
2014-02-23 20:48:36 +0000

    Removed tabs

--- content/autotrader.js
+++ content/autotrader.js
@@ -1,11 +1,11 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
-	var result = $('[name="bannerMetaData"]').attr('content').match(/make=(.+),model=(.+),mileage.*/);
+    var result = $('[name="bannerMetaData"]').attr('content').match(/make=(.+),model=(.+),mileage.*/);
 
     sendResponse({
-	 	url: location.href,
-	 	price: parseInt($('#price').text().substring(1).replace(',', '')),
-	 	name: result[1] + ' ' + result[2],
-	 	image: $('#fpa-showroomCover').attr('src')
- 	});
+        url: location.href,
+        price: parseInt($('#price').text().substring(1).replace(',', '')),
+        name: result[1] + ' ' + result[2],
+        image: $('#fpa-showroomCover').attr('src')
+    });
 });
