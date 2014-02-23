52cfcc24992b16fc4da7e8a58cc03aeefa7c5d3f
Alejandro Tardín <alejandro@tardin.com>
2014-02-23 22:59:47 +0000

    Added more text to the installation message

--- background.js
+++ background.js
@@ -2,21 +2,21 @@ var trello = new Trello();
 
 trello.auth(function(profile) {
 
-	$.getJSON('https://c2t.herokuapp.com/user', {id: profile.id}).success(function(user) {
-		trello.setData(user);
-	}).error(function() {
-		trello.createBoard(function(data) {                        
+    $.getJSON('https://c2t.herokuapp.com/user', {id: profile.id}).success(function(user) {
+        trello.setData(user);
+    }).error(function() {
+        trello.createBoard(function(data) {                        
             $.post('https://c2t.herokuapp.com/user', {
-            	id: profile.id,
-            	board: data.board,
-            	list: data.list
+                id: profile.id,
+                board: data.board,
+                list: data.list
             }).success(function() {
-            	localStorage['installed'] = true;            	
-            	chrome.tabs.create({url: "options/options.html"});
+                localStorage['installed'] = true;               
+                chrome.tabs.create({url: "options/options.html"});
             });
         });
-	});
-	
+    });
+    
     chrome.runtime.onMessage.addListener(function(car, sender, sendResponse) {
         trello.createCard(car, function() {
             chrome.runtime.sendMessage();
