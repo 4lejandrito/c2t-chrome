82dff92f1961c4f91aa64c5a5df12f7f4714ec8d
Alejandro Tardín <alejandro@tardin.com>
2014-02-23 22:53:37 +0000

    Integrated with c2t server

--- options/options.js
+++ options/options.js
@@ -1,16 +1,9 @@
-var trello = new Trello();
+var data = new Trello().getData();
 
-trello.auth(function(user) {    
-    var data = trello.getData();    
-    if (data.board) {
-        $('#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
-        $('#loading').addClass('hide');
-    } else {
-        trello.createBoard(function(data) {                        
-            $('#createBoard,#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
-            $('#loading').addClass('hide');
-        });        
-    }
-}, function() {
-    $('#login').removeClass('hide');
-});
+$('#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
+$('#loading').addClass('hide');
+
+if (localStorage['installed']) {
+    $('#createBoard').removeClass('hide');
+    delete localStorage['installed'];
+}
