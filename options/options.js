6a257520773ff9ce501fb352cc3a69cee1889425
Alejandro Tardín <alejandro@tardin.com>
2014-02-23 18:34:47 +0000

    Workaround for list deletion and loading bug fix

--- options/options.js
+++ options/options.js
@@ -1,13 +1,14 @@
 var trello = new Trello();
 
-trello.auth(function(user) {
-    $('#loading').addClass('hide');
+trello.auth(function(user) {    
     var data = trello.getData();    
     if (data.board) {
         $('#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
+        $('#loading').addClass('hide');
     } else {
         trello.createBoard(function(data) {                        
             $('#createBoard,#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
+            $('#loading').addClass('hide');
         });        
     }
 }, function() {
