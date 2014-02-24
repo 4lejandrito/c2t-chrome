72d6fd5c8ddac8987931d15d6e0fde08e77f81f2
Alejandro Tardín <alejandro@tardin.com>
2014-02-24 23:15:17 +0000

    Better installation message

--- background.js
+++ background.js
@@ -5,18 +5,17 @@ trello.auth(function(profile) {
     $.getJSON('https://c2t.herokuapp.com/user', {id: profile.id}).success(function(user) {
         trello.setData(user);
     }).error(function() {
-        trello.createBoard(function(data) {                        
+        trello.createBoard(function(data) {
             $.post('https://c2t.herokuapp.com/user', {
                 id: profile.id,
                 board: data.board,
                 list: data.list
-            }).success(function() {
-                localStorage['installed'] = true;               
+            }).success(function() {                
                 chrome.tabs.create({url: "options/options.html"});
             });
         });
     });
-    
+
     chrome.runtime.onMessage.addListener(function(car, sender, sendResponse) {
         trello.createCard(car, function() {
             chrome.runtime.sendMessage();
--- options/options.js
+++ options/options.js
@@ -3,7 +3,7 @@ var data = new Trello().getData();
 $('#board').removeClass('hide').find('a').attr('href', 'https://trello.com/b/' + data.board);
 $('#loading').addClass('hide');
 
-if (localStorage['installed']) {
+if (!localStorage['installed']) {
     $('#createBoard').removeClass('hide');
-    delete localStorage['installed'];
+    localStorage['installed'] = true;
 }
